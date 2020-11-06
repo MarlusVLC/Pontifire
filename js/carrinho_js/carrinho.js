f054950ffc640a99d6b6765d489d9df9de643408
Marlus Vinicius <marlusvinicius2014@outlook.com>
2020-11-06 10:22:33 -0300

    Nenhuma mudanca. Somente confirmacao do pull

--- js/carrinho_js/carrinho.js
+++ js/carrinho_js/carrinho.js
@@ -23,24 +23,24 @@ var bancoDadosCarrinho = window.localStorage;
         var conteudo = "";
 
          conteudo += '<div class="divListaCarrinho">';
-         conteudo += '<hr/><hr/>';
-         conteudo += '<div class="divImagemCarrinho">';
-         conteudo += '<img src="../img/catalogo_img/'+ arrayCarrinho[i][1] +  '">';
-         conteudo += '</div>';
-         conteudo += '<div class="divJogosCarrinho">';
-         conteudo += '<h3>' + arrayCarrinho[i][0] + '</h3>';
-         conteudo += '</div>';
-         conteudo += '<div class="divPreco" >';
-         conteudo += '<input class="divPreco1" type="text" id="bPreco" readonly="readonly" value = "R$ ' +  arrayCarrinho[i][2]  +'">  ';
-         conteudo += '</div>';
-         conteudo += '<div class="divQtd" >';
-         conteudo += '<input  data-quantity="pop" class="divQtd1" type="submit" id="bMenos" value= "-" readonly="readonly"> ';
-         conteudo += '<input  autocomplete="off" data-quantity="input" class="divQtd1" type="tel" id="bQtd" value= "' +  arrayCarrinho[i][5]  +'" readonly="readonly"> ';
-         conteudo += '<input  data-quantity="push" class="divQtd1" type="submit" id="bMais" value= "+" readonly="readonly"> ';
-         conteudo += '</div>';
-         conteudo += '<div class="divTotal" >';
-         conteudo += '<input class="divTotal1" type="text" id="btotal" readonly="readonly"> ';
-         conteudo += '</div>';
+         conteudo +=    '<hr/><hr/>';
+         conteudo +=    '<div class="divImagemCarrinho">';
+         conteudo +=       '<img src="../img/catalogo_img/'+ arrayCarrinho[i][1] +  '">';
+         conteudo +=    '</div>';
+         conteudo +=    '<div class="divJogosCarrinho">';
+         conteudo +=       '<h3>' + arrayCarrinho[i][0] + '</h3>';
+         conteudo +=    '</div>';
+         conteudo +=    '<div class="divPreco" >';
+         conteudo +=       '<input class="divPreco1" type="text" id="bPreco" readonly="readonly" value = "R$ ' +  arrayCarrinho[i][2]  +'">  ';
+         conteudo +=    '</div>';
+         conteudo +=    '<div class="divQtd" >';
+         conteudo +=       '<input  data-quantity="pop" class="divQtd1" type="submit" id="bMenos" value= "-" readonly="readonly"> ';
+         conteudo +=       '<input  autocomplete="off" data-quantity="input" class="divQtd1" type="tel" id="bQtd" value= "' +  arrayCarrinho[i][5]  +'" readonly="readonly"> ';
+         conteudo +=       '<input  data-quantity="push" class="divQtd1" type="submit" id="bMais" value= "+" readonly="readonly"> ';
+         conteudo +=    '</div>';
+         conteudo +=    '<div class="divTotal" >';
+         conteudo +=       '<input class="divTotal1" type="text" id="btotal" readonly="readonly"> ';
+         conteudo +=    '</div>';
          conteudo += '</div>';
 
         $("#tabelaCarrinho").append(conteudo)
@@ -60,6 +60,7 @@ var bancoDadosCarrinho = window.localStorage;
       for (var i = 0; i < arrayCarrinho.length; i++){
          var valor1 = arrayCarrinho[i][5];
          //console.log(valor1);
+         
          var valor1 = valor1 + 1;
          arrayCarrinho[i].splice(5, 1, valor1);
          $("#bQtd").val(valor1);
