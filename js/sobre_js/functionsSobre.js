60f7b4a22db4e8aaf6bad1f8857b196bc62615bc
ViniciusTaborda <vinieducosta+1@outlook.com>
2020-11-10 21:29:01 -0300

    Mapa finalizado

--- js/sobre_js/functionsSobre.js
+++ js/sobre_js/functionsSobre.js
@@ -1,8 +1,18 @@
- let map;
+var mapa;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+
+  var configuracoes = {
+    center: {lat: -25.45212754, lng: -49.25276682},
+    zoom: 15
+  }
+      
+  mapa = new google.maps.Map(document.getElementById('map'), configuracoes);
+
+  var marcador = new google.maps.Marker({
+    position: {lat: -25.45212754, lng: -49.25276682},
+    title: "PUCPR",
+    map: mapa
   });
+
 }
