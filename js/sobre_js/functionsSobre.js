87e0e96cc7dedeb914ece85acfe627a46d7a20bb
Marlus Vinicius <marlusvinicius2014@outlook.com>
2020-11-14 19:26:44 -0300

    Área de depoimentos finalizada

--- js/sobre_js/functionsSobre.js
+++ js/sobre_js/functionsSobre.js
@@ -1,3 +1,24 @@
+$(document).ready(function(){
+  // TODO Arrumar a funcao que executa o redirecionamento à partir de uma lista
+  // $('.depo').click(function(){
+  //   alert($(this).attr("id"));
+  //   var id = $(this).attr("id");
+  //   redirecionarDepoentes(id);
+  // })
+
+  $("#depoNeitherNathan").click(function(){
+    window.open("https://neithernathan.itch.io/")
+  })
+
+  $("#depoPlaayL").click(function(){
+    window.open("https://plaayl.itch.io/")
+  })
+
+  $("#depoMahouTangerino").click(function(){
+    window.open("https://mahou.itch.io/")
+  })
+})
+
 var mapa;
 
 function initMap() {
@@ -16,3 +37,27 @@ function initMap() {
   });
 
 }
+
+
+function redirecionarDepoentes(id){
+
+    depoentesIdURL = [
+                      ["#depoNathan", "https://neithernathan.itch.io/"],
+                      ["#depoLuiz",   "https://plaayl.itch.io/" ],
+                      ["#depoIsrael", "https://mahou.itch.io/" ],
+                     ];
+
+    
+    for (var i = 0; i < depoentesIdURL.length; i++){
+      if (depoentesIdURL[i].includes(id)){
+        window.open(depoentesIdURL[depoentesIdURL.indexOf(id)][1])
+        return;
+      }
+    }  
+    
+    // alert("Id nao encontrado :(.")
+    // alert(id);
+    console.log(id);
+}
+
+
