e9056c255ac93a2b208849dcb5186e7c3b916c0c
Marlus Vinicius <marlusvinicius2014@outlook.com>
2020-11-18 23:23:20 -0300

    Cabecalho e menu em posicao fixa. href corrigido. divInfo aumentada

--- js/frontpage_js/frontFunctions.js
+++ js/frontpage_js/frontFunctions.js
@@ -15,11 +15,14 @@ $(document).ready(function(){
     storage = window.localStorage;
 
     if (window.location.pathname.includes("index.html")){
+        
+        scrollFix = -250;
 
         var mensagemRec = storage.getItem("mensagem_de_transicao");
 
         if (mensagemRec == "GoToCatalogo"){ document.getElementById("tituloCatalogo").scrollIntoView();  }
         else if (mensagemRec == "GoToVideo"){ document.getElementById("tituloVideo").scrollIntoView();  }
+        window.scrollBy(0, scrollFix);
         storage.setItem("mensagem_de_transicao", " ");
 
 
@@ -32,12 +35,14 @@ $(document).ready(function(){
             // window.location.assign("paginas/catalogo1.html");
             var elem = document.getElementById("tituloCatalogo");
             elem.scrollIntoView();
+            window.scrollBy(0, scrollFix);
         })
 
         $(".aAboutUs").click(function(){
             // window.location.assign("paginas/Sobre.html");
             var elem = document.getElementById("tituloVideo");
             elem.scrollIntoView();
+            window.scrollBy(0, scrollFix);
         })
         
         $(".imgLogin").click(function(){
@@ -58,11 +63,13 @@ $(document).ready(function(){
             // window.location.assign("../paginas/catalogo1.html");
             storage.setItem("mensagem_de_transicao", "GoToCatalogo");
             window.location.assign("../index.html");
+            window.scrollBy(0, scrollFix);
         })
         $(".aAboutUs").click(function(){
             // window.location.assign("../paginas/Sobre.html");
             storage.setItem("mensagem_de_transicao", "GoToVideo");
             window.location.assign("../index.html");
+            window.scrollBy(0, scrollFix);
         })
         $(".imgLogin").click(function(){
             window.location.assign("../paginas/login.html");
